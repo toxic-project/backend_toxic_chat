607ff0bd397dce378624862e4bdbcaa10da9f484
Savitar465 <55471147+Savitar465@users.noreply.github.com>
2021-03-29 22:31:30 -0400

    First commit

---
+++ src/main/java/com/ucb/bo/ToxicChat/ToxicChatApplication.java
@@ -0,0 +1,13 @@
+package com.ucb.bo.ToxicChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToxicChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToxicChatApplication.class, args);
+	}
+
+}

dd0534f87779c972d8d7cab196286893ded6012f
Savitar465 <55471147+Savitar465@users.noreply.github.com>
2021-04-13 19:15:54 -0400

    key getters fixed

--- src/main/java/com/ucb/bo/ToxicChat/util/KeyAmazon.java
+++ src/main/java/com/ucb/bo/ToxicChat/util/KeyAmazon.java
@@ -6,4 +6,12 @@ public class KeyAmazon {
     private String key="xxxxxxxxxx";
     private String secret="xxxxxxxxxx";
 
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
 }
